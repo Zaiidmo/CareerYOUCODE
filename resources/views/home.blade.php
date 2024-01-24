5bb855383f64333275a887344b84a2aca0bc04fb
Zaiid Moumnii <zaiidmoumnii@gmail.com>
2024-01-25 00:25:01 +0100

    Setting up Environment

---
+++ resources/views/home.blade.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="{{ asset('css/app.css') }}">
+    <title>CareerYOUCODE</title>
+</head>
+
+<body>
+    <h1 class="text-6xl text-red-500"> Hello World </h1>
+</body>
+
+</html>
