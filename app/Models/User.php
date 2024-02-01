78fdba336290c08e4ec54080ca0ca810a5d89faa
Zaiidmo <Zaiidmoumnii@gmail.com>
2024-02-01 11:26:05 +0100

    Editing User Model

--- app/Models/User.php
+++ app/Models/User.php
@@ -13,4 +13,27 @@ class User extends Model
         'email',
         'role',
     ];
+    /**
+     * The attributes that should be hidden for serialization.
+     *
+     * @var array<int, string>
+     */
+    protected $hidden = [
+        'password',
+        'remember_token',
+    ];
+
+    /**
+     * The attributes that should be cast.
+     *
+     * @var array<string, string>
+     */
+    protected $casts = [
+        'email_verified_at' => 'datetime',
+        'password' => 'hashed',
+    ];
+    public function Announcements(){
+        return $this->hasMany(Announcement::class);
+    }
 }
+
