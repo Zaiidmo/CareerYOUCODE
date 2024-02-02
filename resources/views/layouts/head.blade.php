6d016e5df16ba3594bd1d6aa11ff9d26bfa36aa9
Zaiidmo <Zaiidmoumnii@gmail.com>
2024-02-02 10:28:15 +0100

    Adding a favicon

--- resources/views/layouts/head.blade.php
+++ resources/views/layouts/head.blade.php
@@ -8,7 +8,7 @@
     <meta name="author" content="Zaiid Moumnii">
     <meta name="description" content="CarrerYOUCODE .. JobDating Events By YOUCODE School">
     <link rel="stylesheet" href="{{ asset('css/app.css') }}">
-    {{-- @vite('resources/css/app.css') --}}
+    <link rel="shortcut icon" href="/images/favicon.png" type="image/x-icon">
     <title>@yield('title')</title>
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
