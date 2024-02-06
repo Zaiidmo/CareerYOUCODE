a83d307520d1cfe57cb84de93efe02625161e4c4
Zaiidmo <zaiidmoumnii@gmail.com>
2024-02-06 17:04:21 +0100

    Creating Pivot Tables for Skills

--- database/factories/SkillFactory.php
+++ database/factories/SkillFactory.php
@@ -2,6 +2,7 @@
 
 namespace Database\Factories;
 
+use App\Models\Skill;
 use Illuminate\Database\Eloquent\Factories\Factory;
 
 /**
@@ -9,6 +10,7 @@ use Illuminate\Database\Eloquent\Factories\Factory;
  */
 class SkillFactory extends Factory
 {
+    protected $model = Skill::class;
     /**
      * Define the model's default state.
      *
@@ -17,7 +19,7 @@ class SkillFactory extends Factory
     public function definition(): array
     {
         return [
-            //
+            'name' => $this->faker->unique()->word(),
         ];
     }
 }
