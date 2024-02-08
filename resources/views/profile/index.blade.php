37672fb101efbe65ba992a4dd28d687a439d2b96
Zaiidmo <zaiidmoumnii@gmail.com>
2024-02-08 09:17:51 +0100

    Editing Profile styles

--- resources/views/profile/index.blade.php
+++ resources/views/profile/index.blade.php
@@ -80,7 +80,7 @@
 
                                 </form>
                             </div>
-                            <div class="flex flex-row">
+                            <div class="flex flex-row flex-wrap gap-3">
                                 @foreach ($user->skills as $skill)
                                     <span
                                         class="bg-gray-100 text-gray-800  text-xs font-medium me-2 px-2.5 py-2 rounded-full dark:bg-gray-700 dark:text-white border border-gray-500">{{ $skill->name }}</span>
